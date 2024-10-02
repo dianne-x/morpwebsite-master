4dd1a1c54826622c65850e33c438ba3b926e0963
Bevíz László <bevizlaci@gmail.com>
2024-10-02 20:37:45 +0200

    fixed console error with navbar replacing activeClassName

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../img/morp_light_horizontal.png'
+import logo from '../img/morp_light_horizontal.png';
 
 const Navbar = () => {
   const [exploreOpen, setExploreOpen] = useState(false);
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo-container">
-          <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" />
       </div>
       <ul className="nav-links">
         <li>
@@ -25,7 +25,7 @@ const Navbar = () => {
           {exploreOpen && (
             <ul className="dropdown">
               <li>
-                <NavLink to="/" exact activeClassName="active">
+                <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
                   Home
                 </NavLink>
               </li>
@@ -53,39 +53,42 @@ const Navbar = () => {
           {playerSrchOpen && (
             <ul className="dropdown">
               <li>
-                <NavLink to="/player-search" activeClassName="active">Search by name</NavLink>
+                <NavLink to="/player-search" className={({ isActive }) => (isActive ? "active" : "")}>
+                  Search by name
+                </NavLink>
               </li>
               <li>Search by ID</li>
             </ul>
           )}
         </li>
-
         <li>
           <button onClick={() => toggleDropdown(setRulesOpen)}>
             Rules
           </button>
           {RulesOpen && (
             <ul className="dropdown">
-                <li>
-                    <NavLink to="/rules" activeClassName="active">Game Rules</NavLink>
-                </li>
               <li>
-                <NavLink to="/combat-rules" activeClassName="active">
+                <NavLink to="/rules" className={({ isActive }) => (isActive ? "active" : "")}>
+                  Game Rules
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/combat-rules" className={({ isActive }) => (isActive ? "active" : "")}>
                   Combat Rules
                 </NavLink>
-                </li>
+              </li>
               <li>
-                <NavLink to="/character-rules" activeClassName="active">
+                <NavLink to="/character-rules" className={({ isActive }) => (isActive ? "active" : "")}>
                   Character Rules
                 </NavLink>
               </li>
             </ul>
-            )}
+          )}
         </li>
       </ul>
       <div className="login-button">
-        <NavLink to="/login" activeClassName="active">
-        <button>Login</button>
+        <NavLink to="/login" className={({ isActive }) => (isActive ? "active" : "")}>
+          <button>Login</button>
         </NavLink>
       </div>
     </nav>
