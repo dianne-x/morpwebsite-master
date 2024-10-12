69511ccc57213b89d6cfd8c1700ef6cb6e51c101
Bevíz László <bevizlaci@gmail.com>
2024-10-13 01:14:28 +0200

    Refactor FriendList component and add FriendList to TopBar

---
+++ src/components/App/FriendList.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import TopBarButton from './TopBarButton';
+
+const initialFriends = [
+    { id: 1, icon: '👨', name: 'Josh' },
+    { id: 2, icon: '👩', name: 'Alice' },
+    { id: 3, icon: '👧', name: 'Lily' },
+    { id: 4, icon: '👦', name: 'Tom' },
+    { id: 5, icon: '👵', name: 'Granny' },
+    { id: 6, icon: '👴', name: 'Grandpa' },
+    { id: 7, icon: '👶', name: 'Baby' },
+    { id: 8, icon: '🧒', name: 'Kid' },
+    { id: 9, icon: '🧑', name: 'Adult' },
+    { id: 11, icon: '👨‍🦱', name: 'Curly' },
+    { id: 13, icon: '👨‍🦲', name: 'Bald' },
+    { id: 15, icon: '👨‍🦳', name: 'Gray' },
+    { id: 17, icon: '👱‍♂️', name: 'Blond' },
+    { id: 19, icon: '🧔', name: 'Beard' },
+    { id: 20, icon: '👨‍🦰', name: 'Redhead' },
+    { id: 22, icon: '👨‍🦱', name: 'Curly' }
+];
+
+const FriendList = ({ onFriendClick }) => {
+  const [friends, setFriends] = useState(initialFriends);
+
+  return (
+    <div className="server-list">
+      {friends.map((friend) => (
+        <TopBarButton key={friend.id} icon={friend.icon} onClick={() => onFriendClick(friend)} title={friend.name} />
+      ))}
+    </div>
+  );
+};
+
+// Prop validation
+FriendList.propTypes = {
+  onFriendClick: PropTypes.func.isRequired,
+};
+
+export default FriendList;
--- src/components/App/TopBar.js
+++ src/components/App/TopBar.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import '../../style/TopBar.scss';
+import FriendList from './FriendList';
 import ServerList from './ServerList';
 import ServerCreationForm from './createServerForm'; // Import the form modal component
 import TopBarButton from './TopBarButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faAdd, faUser, faUsers, faCompass } from '@fortawesome/free-solid-svg-icons'
 
 const Topbar = ({ onServerClick }) => {
   const topbarRef = useRef(null);
   const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const [isServersSelected, setIsServersSelected] = useState(true);
 
   useEffect(() => {
     const handleWheel = (event) => {
@@ -36,16 +41,22 @@ const Topbar = ({ onServerClick }) => {
   return (
     <div className="topbar" ref={topbarRef}>
       <div className='home-list'>
-        <TopBarButton icon="🏠" title="Home" onClick={() => onServerClick({ id: 1, name: 'Home' })} />
-        <TopBarButton icon="+" title="Add Server" onClick={() => handleFormOpen()} />
+        <TopBarButton icon={<FontAwesomeIcon icon={faHome} />} title="Home" onClick={() => onServerClick({ id: 1, name: 'Home' })} />
+        <TopBarButton icon={<FontAwesomeIcon icon={faAdd} />} title="Add Server" onClick={() => handleFormOpen()} />
       </div>
       
-      <ServerList onServerClick={onServerClick} onCreateServerClick={handleFormOpen} />
+      {isServersSelected ? 
+        <ServerList onServerClick={onServerClick} onCreateServerClick={handleFormOpen} />
+         : 
+        <FriendList onFriendClick={() => {}} />
+      }
+        
+      
       {isFormOpen && <ServerCreationForm onClose={handleFormClose} />}
 
       <div className='user-list'>
-        <TopBarButton icon="🔔" title="Friendo" />
-        <TopBarButton icon="👤" title="Profile" />
+        <TopBarButton icon={<FontAwesomeIcon icon={(isServersSelected ? faCompass : faUsers)} />} title="Friend" onClick={() => setIsServersSelected(!isServersSelected)} />
+        <TopBarButton icon={<FontAwesomeIcon icon={faUser} />} title="Profile" />
       </div>
     </div>
   );
