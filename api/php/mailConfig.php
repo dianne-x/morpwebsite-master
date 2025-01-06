5c6ecaff42c5326c393ce92d058e1af993d44be0
Bevíz László <bevizlaci@gmail.com>
2025-01-06 09:20:31 +0100

    feat: add PHPMailer dependency and configure email settings in mailConfig.php

---
+++ api/php/mailConfig.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('MAIL_HOST', 'smtp.gmail.io');
+define('USERNAME', 'morpverse@gmail.com');
+define('PASSWORD', '16digitappcode'); // need to replace this with app password !!!!!!!! (maybe put to dotenv later)
+define('SEND_FROM', 'morpverse@gmail.com');
+define('SEND_FROM_NAME', 'Team MORP');
+define('REPLY_TO', 'morpverse@gmail.com');
+define('REPLY_TO_NAME', 'Team MORP');
