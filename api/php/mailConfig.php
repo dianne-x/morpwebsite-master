c446af4f2e9a03b8148c7d915f7b157d972aeb71
Bevíz László <bevizlaci@gmail.com>
2025-01-06 21:36:51 +0100

    feat: integrate PHPMailer for email functionality in registration and password request processes; update mail configuration

--- api/php/mailConfig.php
+++ api/php/mailConfig.php
@@ -1,9 +1,50 @@
 <?php 
 
-define('MAIL_HOST', 'smtp.gmail.io');
+define('MAIL_HOST', 'smtp.gmail.com');
 define('USERNAME', 'morpverse@gmail.com');
-define('PASSWORD', '16digitappcode'); // need to replace this with app password !!!!!!!! (maybe put to dotenv later)
+define('PASSWORD', 'cvmc nbko giaj yira'); // (maybe put to dotenv later)
 define('SEND_FROM', 'morpverse@gmail.com');
 define('SEND_FROM_NAME', 'Team MORP');
 define('REPLY_TO', 'morpverse@gmail.com');
 define('REPLY_TO_NAME', 'Team MORP');
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require __DIR__ . '/../vendor/autoload.php'; 
+
+function sendMail($to, $subject, $body) {
+
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+
+    $mail->Host = MAIL_HOST;
+    $mail->Username = USERNAME;
+    $mail->Password = PASSWORD;
+
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom(SEND_FROM, SEND_FROM_NAME);
+
+    $mail->addAddress($to);
+    $mail->addReplyTo(REPLY_TO, REPLY_TO_NAME);
+
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    $mail->AltBody = strip_tags($body);
+
+    try {
+        $mail->send();
+    } catch (Exception $e) {
+        error_log("Mailer Error: " . $mail->ErrorInfo);
+        throw new Exception("Email could not be sent.");
+    }
+
+}
+
