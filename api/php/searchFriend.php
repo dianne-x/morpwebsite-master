d00230dbab2fd4482b979180e3a3744a8ed67b84
Bobák Kornél <143836544+dianne-x@users.noreply.github.com>
2025-02-24 09:31:06 +0100

    feat: add friend search functionality and integrate AddFriend component in CreateJoinPanel

---
+++ api/php/searchFriend.php
@@ -0,0 +1,34 @@
+<?php
+require 'dbConnection.php'; // Include the database connection file
+
+// CORS
+header('Access-Control-Allow-Origin: http://localhost:3000'); // Allow requests from your React app
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); // Allow specific HTTP methods
+header('Access-Control-Allow-Headers: Content-Type, Authorization'); // Allow specific headers
+header('Content-Type: application/json'); // Ensure the response is JSON
+
+if ($conn->connect_error) {
+    $response['error'] = "Connection failed: " . $conn->connect_error;
+    echo json_encode($response);
+    exit();
+}
+
+$name = $_GET['name'];
+
+$sql = "SELECT uid, name FROM users WHERE name LIKE ?";
+$stmt = $conn->prepare($sql);
+$searchTerm = "%" . $name . "%";
+$stmt->bind_param("s", $searchTerm);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$users = [];
+while ($row = $result->fetch_assoc()) {
+    $users[] = $row;
+}
+
+echo json_encode($users);
+
+$stmt->close();
+$conn->close();
+?>
