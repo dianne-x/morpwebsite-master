7e5af19dbd588ad7df45a3f63e27b7b70393cc9e
Bevíz László <bevizlaci@gmail.com>
2025-03-21 14:04:56 +0100

    feat(WantedElements): add form for selecting needed elements with checkboxes

--- client/src/components/App/Server/ServerSettings/WantedElements.js
+++ client/src/components/App/Server/ServerSettings/WantedElements.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 const WantedElements = () => {
+    const neededElements = [
+        { name: 'Birthdate' },
+        { name: 'Deathdate' },
+        { name: 'Resurrected date' },
+        { name: 'Species' },
+        { name: 'Occupation' },
+        { name: 'Affiliation' },
+        { name: 'Nationality' },
+        { name: 'Powers' },
+        { name: 'Weaknesses' },
+        { name: 'Used item' },
+        { name: 'Family' },
+        { name: 'Universe' },
+        { name: 'Fan Cast' },
+    ]
     return (
         <div>
             <h1>Wanted Elements</h1>
-            <p>This is a plain React component.</p>
+            <p>In this section you can decide...</p>
+            <form>
+                {neededElements.map((element, index) => (
+                    <div key={index} className='needed-element'>
+                        <label htmlFor={element.name}>{element.name}</label>
+                        <input type='checkbox' id={element.name} name={element.name} />
+                    </div>
+                ))}
+                <button type='submit'>Save Changes</button>
+            </form>
         </div>
     );
 };
